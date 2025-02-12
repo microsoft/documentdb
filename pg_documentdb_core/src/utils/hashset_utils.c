e0c6b91e6eaaab6440d763f276cac1301c4f9ef2
Marcelo Fonseca <marcrod@microsoft.com>
2025-02-11 23:56:26 -0300

    merge optimization

--- pg_documentdb_core/src/utils/hashset_utils.c
+++ pg_documentdb_core/src/utils/hashset_utils.c
@@ -80,14 +80,14 @@ PgbsonElementHashEntryCompareFunc(const void *obj1, const void *obj2, Size objsi
 	const PgbsonElementHashEntry *hashEntry1 = obj1;
 	const PgbsonElementHashEntry *hashEntry2 = obj2;
 
-	int minLength = Min(hashEntry1->element.pathLength, hashEntry2->element.pathLength);
-	int result = strncmp(hashEntry1->element.path, hashEntry2->element.path, minLength);
-
-	if (result == 0)
-	{
-		return hashEntry1->element.pathLength - hashEntry2->element.pathLength;
+	// First, compare the lengths
+	int lenDiff = hashEntry1->element.pathLength - hashEntry2->element.pathLength;
+	if (lenDiff != 0) {
+		return lenDiff;
 	}
-	return result;
+
+	// If lengths are equal, compare the paths
+	return strncmp(hashEntry1->element.path, hashEntry2->element.path, hashEntry1->element.pathLength);
 }
 
 
@@ -219,15 +219,12 @@ PgbsonElementOrderedHashCompareFunc(const void *obj1, const void *obj2, Size obj
 	const PgbsonElementHashEntryOrdered *hashEntry1 = obj1;
 	const PgbsonElementHashEntryOrdered *hashEntry2 = obj2;
 
-	int minPathLength = Min(hashEntry1->element.pathLength,
-							hashEntry2->element.pathLength);
-	int result = strncmp(hashEntry1->element.path, hashEntry2->element.path,
-						 minPathLength);
-
-	if (result == 0)
-	{
-		return hashEntry1->element.pathLength - hashEntry2->element.pathLength;
+	// First, compare the lengths
+	int lenDiff = hashEntry1->element.pathLength - hashEntry2->element.pathLength;
+	if (lenDiff != 0) {
+		return lenDiff;
 	}
 
-	return result;
+	// If lengths are equal, compare the paths
+	return strncmp(hashEntry1->element.path, hashEntry2->element.path, hashEntry1->element.pathLength);
 }
